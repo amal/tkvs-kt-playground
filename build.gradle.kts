47c352da417d69d210df2cb4cee112056bc2287c
Artyom Shendrik <artyom.shendrik@gmail.com>
2024-01-26 17:00:39 +0000

    feat: shadow jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,28 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.detekt)
     alias(libs.plugins.fluxo.conf)
+    alias(libs.plugins.shadow)
 }
 
-group = "kt.tkvs"
+val groupName = "kt.tkvs"
+group = groupName
 version = libs.versions.version.toString()
 
 setupKotlin(
     config = {
         enableApiValidation = false
-    }
+    },
 )
 
 dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks {
+    test { useJUnitPlatform() }
+
+    jar { manifest.attributes["Main-Class"] = "$groupName.MainKt" }
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
+        minimize()
+    }
 }
